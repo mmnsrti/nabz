72ac7f469f95cbedfe553a157719a6e6fde1738c
mmnsrti <mmnsrti@gmail.com>
2024-07-24 15:44:56 +0330

    "Updated layout.tsx to add ThemeProvider and modified font and class definitions; removed text-white class from Button in page.tsx"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,18 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
-const inter = Inter({ 
-  subsets: ["latin"], 
-  weight:["300","400","500","700"],
-  variable:'--font-sans'
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
   title: "Nabz",
-  description: "A healthcare management system / یک سیستم مدیریت بهداشت و درمان",
+  description:
+    "A healthcare management system / یک سیستم مدیریت بهداشت و درمان",
 };
 
 export default function RootLayout({
@@ -21,7 +23,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased',inter.variable)}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          inter.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
