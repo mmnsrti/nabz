7e69130542b7f417a90445cdb85dd83c9190ee64
mmnsrti <mmnsrti@gmail.com>
2024-07-24 17:12:10 +0330

    "Commented out Image component in Home page and refactored PatientForm component to use react-hook-form with zod validation"

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const PatientForm = () => {
-  return (
-    <div>
-      PatientForm
-    </div>
-  )
-}
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
 
-export default PatientForm
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Patient</h1>
+          <p className="text-dark-700">Schedule your fist appointment</p>
+        </section>
+   
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+export default PatientForm;
