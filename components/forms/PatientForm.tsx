9fd6795fd32674498f962ecdc09c7889dfb3a42d
mmnsrti <mmnsrti@gmail.com>
2024-07-27 16:14:28 +0330

    "Updated settings.json, PatientForm.tsx, and CustomFormFields.tsx; removed CustomFormFields.tsx; added node-appwrite dependency"

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,11 +5,11 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import CustomFormFields from "../ui/CustomFormFields";
+import CustomFormFields from "../CustomFormFields";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import UserFormValidation from "@/lib/validation";
-
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -21,7 +21,8 @@ export enum FormFieldType {
   SKELETON = "skeleton",
 }
 const PatientForm = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
   // 1. Define your form.
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
@@ -33,17 +34,26 @@ const PatientForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
         <section className="mb-12 space-y-4">
-          <h1 className="header">Patient</h1>
+          <h1 className="header">Hi</h1>
           <p className="text-dark-700">Schedule your fist appointment</p>
         </section>
         <CustomFormFields
